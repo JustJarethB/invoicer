0770a63d511cbea4524ced89881bb710c9d6f513
Jareth Bower <31932204+JustJarethB@users.noreply.github.com>
2025-07-01 00:24:51 +0100

    add basic navbar

---
+++ app/layouts/navbar.tsx
@@ -0,0 +1,32 @@
+import { ArrowRightIcon } from "@heroicons/react/16/solid";
+import type { PropsWithChildren } from "react";
+import { Outlet, NavLink, type NavLinkProps } from "react-router";
+
+const StyledLink = (props: PropsWithChildren<Omit<NavLinkProps, 'children'>>) => {
+    return (
+        <NavLink
+            {...props}
+            className={({ isActive }) =>
+                `block px-4 py-2 rounded hover:bg-gray-200 dark:hover:bg-gray-700 ${isActive ? 'dark:bg-gray-600 bg-gray-300  font-bold' : ''}`
+            }
+        >
+            {({ isActive }) => (
+                <div className="flex justify-between items-center">
+                    {props.children}{isActive && <ArrowRightIcon className="h-5 -mr-2" />}
+                </ div>
+            )}
+        </NavLink>
+    );
+}
+
+export default function Navbar() {
+    return <div className="grid grid-cols-12">
+        <nav className="print:collapse col-span-1 bg-gray-100 dark:bg-gray-800 p-4 shadow-md min-h-screen">
+            <StyledLink to="/">Invoice</StyledLink>
+            <StyledLink to="/clients">Clients</StyledLink>
+        </nav>
+        <main className="col-span-11 print:col-span-full">
+            <Outlet />
+        </main>
+    </div>
+}
--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,8 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
 
 export default [
-    index("routes/invoice.tsx"),
-    route("clients", "routes/clients.tsx"),
+    layout("layouts/navbar.tsx", [
+        index("routes/invoice.tsx"),
+        route("clients", "routes/clients.tsx"),
+    ]),
 ] satisfies RouteConfig;
