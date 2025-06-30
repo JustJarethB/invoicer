c8e798a1c09702c5081dd914fb51e62ed817148b
Jareth Bower <31932204+JustJarethB@users.noreply.github.com>
2025-07-01 00:58:58 +0100

    more button styling

--- app/components/home/Button.tsx
+++ app/components/home/Button.tsx
@@ -3,17 +3,18 @@ type ButtonProps = {
     color?: 'primary' | 'secondary' | 'danger' | 'success' | 'default';
     icon?: boolean;
     size?: 'sm' | 'md' | 'lg';
+    outline?: boolean;
 }
 const getColorClassName = (type: ButtonProps['color']) => {
     switch (type) {
         case 'primary':
-            return 'bg-amber-600 hover:bg-amber-700 text-white';
+            return 'bg-amber-600 dark:bg-amber-700 dark:hover:bg-amber-800 hover:bg-amber-700 text-white dark:[.ring]:text-amber-700 [.ring]:text-amber-600';
         case 'secondary':
             return 'bg-gray-600 hover:bg-gray-700 text-white';
         case 'danger':
-            return 'bg-red-600 hover:bg-red-700 text-white';
+            return 'bg-red-600 dark:bg-red-800 hover:bg-red-700 text-white dark:[.ring]:text-red-800 [.ring]:text-red-600';
         case 'success':
-            return 'bg-green-600 hover:bg-green-700 text-white';
+            return 'bg-green-600 dark:bg-green-800 hover:bg-green-700 text-white dark:[.ring]:text-green-800 [.ring]:text-green-600';
         default:
             return 'bg-gray-700 hover:bg-gray-800 text-white';
     }
@@ -31,6 +32,6 @@ const getSizeClassName = (icon: boolean, size: ButtonProps['size']) => {
     }
 }
 
-export const Button = ({ children, onClick, color, icon = false, size = 'md', className, ...rest }: ComponentPropsWithoutRef<'button'> & ButtonProps) => (
-    <button {...rest} className={`${getSizeClassName(icon, size)} font-bold transition-all duration-200 cursor-pointer disabled:cursor-auto disabled:pointer-events-none ${getColorClassName(color)} ${className}`} onClick={onClick}>{children}</button>
+export const Button = ({ children, onClick, color, icon = false, outline = false, size = 'md', className, ...rest }: ComponentPropsWithoutRef<'button'> & ButtonProps) => (
+    <button {...rest} className={`${getSizeClassName(icon, size)} ${outline && "ring !bg-transparent"} font-bold transition-all duration-200 cursor-pointer disabled:cursor-auto disabled:pointer-events-none ${getColorClassName(color)} ${className}`} onClick={onClick}>{children}</button>
 );
