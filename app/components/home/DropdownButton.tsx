47e6113a8a97fcc0dcbe8e209cd2cb56245697d0
Jareth Bower <31932204+JustJarethB@users.noreply.github.com>
2025-06-30 20:56:31 +0100

    ui tweak dropdown button

--- app/components/home/DropdownButton.tsx
+++ app/components/home/DropdownButton.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/16/solid";
+import { Button } from './Button';
 
 
 type Option = { key: string; value: string };
@@ -20,10 +21,9 @@ export const DropdownButton = ({ options, onClick, children }: Props) => {
     return (
         <div className="group">
             <div className="relative">
-                <button
-                    type="button"
+                <Button
                     disabled={options.length === 0}
-                    className={`p-2 px-4 rounded-t-lg ${open || 'rounded-b-lg'} transition-all group-hover:bg-gray-600 ring-2 ring-white group-hover:ring-gray-900 bg-gray-700 text-white font-bold flex items-center`}
+                    className={`rounded-t-lg ${open || 'rounded-b-lg'} flex items-center`}
                     onClick={() => setOpen(!open)}
                 >
                     {children}
@@ -32,15 +32,15 @@ export const DropdownButton = ({ options, onClick, children }: Props) => {
                     ) : (
                         <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" />
                     )}
-                </button>
+                </Button>
                 <div
-                    className={`absolute bg-gray-700 rounded-b-lg transition-all w-full ring-2 ring-white group-hover:ring-gray-900 ${open ? 'opacity-100' : 'opacity-0 pointer-events-none'
+                    className={`absolute bg-gray-700 rounded-lg transition-all w-full border-t-2 border-t-gray-800 overflow-hidden ${open ? 'opacity-100' : 'opacity-0 pointer-events-none'
                         }`}
                 >
                     {options.map(({ key, value }, i) => (
                         <button
                             key={key}
-                            className="text-white text-xs block w-full justify-center px-2 py-1 hover:bg-gray-600"
+                            className="text-white text-xs block w-full justify-center px-2 py-1 hover:bg-gray-800 transition-colors duration-100"
                             type="button"
                             value={value}
                             onClick={() => handleClick(value, i)}
