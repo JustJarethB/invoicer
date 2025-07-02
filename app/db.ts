4c9d66f909ac213c743f0c64754cbf665e3691a3
Jareth Bower <31932204+JustJarethB@users.noreply.github.com>
2025-07-02 02:04:23 +0100

    feat: db get all

--- app/db.ts
+++ app/db.ts
@@ -1,5 +1,17 @@
 import { useMemo } from "react";
 
+const matchPartialKeys = (keys: string[]) => {
+    return Array(localStorage.length).fill(0).map((_, i) => localStorage.key(i)).filter((key): key is NonNullable<typeof key> => {
+        if (!key) return false;
+        try {
+            const parsedKey = JSON.parse(key);
+            return keys.every(k => parsedKey.includes(k));
+        } catch (e) {
+            console.error("Failed to parse localStorage key:", key, e);
+            return false;
+        }
+    })
+}
 const save = async (keys: string[], data: any) => {
     if (typeof localStorage === 'undefined') return false;
     localStorage.setItem(JSON.stringify(keys), JSON.stringify(data));
@@ -11,6 +23,10 @@ const get = async (keys: string[]) => {
     const data = localStorage.getItem(JSON.stringify(keys));
     return data ? JSON.parse(data) : null;
 }
+const getAll = async (keys: string[] = []) => {
+    if (typeof localStorage === 'undefined') return [];
+    return Promise.all(matchPartialKeys(keys).sort().map(keyStr => get(JSON.parse(keyStr))))
+}
 const remove = async (keys: string[]) => {
     if (typeof localStorage === 'undefined') return false;
     localStorage.removeItem(JSON.stringify(keys));
@@ -19,6 +35,7 @@ const remove = async (keys: string[]) => {
 export const db = {
     save,
     get,
+    getAll,
     remove,
 }
 export const useDb = () => useMemo(() => db, []);
