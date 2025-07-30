fd25e5dee71e6eb3bd660a1c9c592ee6067d2a45
Jareth Bower <31932204+JustJarethB@users.noreply.github.com>
2025-07-31 00:39:33 +0100

    fix: tooltip doesn't show when hidden tooltip hovered

--- app/components/Tooltip.tsx
+++ app/components/Tooltip.tsx
@@ -1,7 +1,7 @@
 export const Tooltip = ({ children, title }: { children: React.ReactNode; title: string }) => {
     return (
         <span className="relative group">
-            <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-max max-w-xs p-2 bg-gray-700 text-white text-xs rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200">
+            <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-max max-w-xs p-2 bg-gray-700 text-white text-xs rounded pointer-events-none opacity-0 group-hover:opacity-100 transition-opacity duration-200">
                 {title}
             </span>
             {children}
