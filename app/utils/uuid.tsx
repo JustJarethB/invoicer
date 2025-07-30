8dcb76da80da2386367dfde3586690923c1e3963
Jareth Bower <31932204+JustJarethB@users.noreply.github.com>
2025-07-30 23:20:54 +0100

    feat: uuid

---
+++ app/utils/uuid.tsx
@@ -0,0 +1,7 @@
+// TODO: refine this
+export const randomUUID = (): string => {
+    // Generate a random UUID (version 4)
+    return (1e7.toString() + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+        (Number(c) ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> Number(c) / 4).toString(16)
+    );
+}
