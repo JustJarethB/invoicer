2c7ec2f7b96c314a2bb3ba376ff20758b9b41734
joelnicell <jjnicell@gmail.com>
2025-10-07 14:44:24 +0100

    feat: invoices page semantic html and layout improvements

--- app/components/home/Status.tsx
+++ app/components/home/Status.tsx
@@ -1,5 +1,5 @@
 import { Button } from "./Button"
 
-export const Status = (props: Omit<Parameters<typeof Button>[0], 'classname' | "outline" | "onClick">) => {
-    return <Button {...props} outline className="pointer-events-none" />
+export const Status = (props: Omit<Parameters<typeof Button>[0], 'classname' | "outline">) => {
+    return <Button {...props} outline />
 }
