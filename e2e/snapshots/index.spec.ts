b6009bc79ea77046f3ed42fc4885d735b0fff78c
Jareth Bower <31932204+JustJarethB@users.noreply.github.com>
2025-10-14 14:14:15 +0100

    fix flaky tests better

--- e2e/snapshots/index.spec.ts
+++ e2e/snapshots/index.spec.ts
@@ -7,19 +7,11 @@ for (const colorScheme of ['light', 'dark'] as const) {
     test.describe(`[${colorScheme}] Visual Regression Tests`, () => {
         test.beforeEach(async ({ page }) => {
             page.emulateMedia({ colorScheme });
+            page.clock.setFixedTime(new Date('2001-12-31T12:00:00Z'));
         })
         test(`Invoice page snapshot`, async ({ page, }) => {
             await page.goto('/');
             await page.waitForLoadState('networkidle')
-            const invoiceInput = page.getByTestId('invoice-ref')
-            await invoiceInput.fill(TEST_ID);
-            const taxDateInput = page.getByTestId('tax-date')
-            await taxDateInput.fill(TEST_DATE);
-            const lineItemDate = page.getByTestId('line-items').locator('input').first();
-            lineItemDate
-            await page.evaluate(() => {
-                (document.querySelector('[data-testid="line-items"] input[type="date"]') as HTMLInputElement).value = '2001-12-31';
-            })
             const screenshot = await page.screenshot({ fullPage: true });
             expect(screenshot).toMatchSnapshot(`homepage-${colorScheme}.png`);
         });
@@ -43,10 +35,6 @@ for (const colorScheme of ['light', 'dark'] as const) {
             await page.setViewportSize({ width: 794, height: 1123 });
             await page.goto('/');
             await page.waitForLoadState('networkidle')
-            const invoiceInput = page.getByTestId('invoice-ref')
-            await invoiceInput.fill(TEST_ID);
-            const taxDateInput = page.getByTestId('tax-date')
-            await taxDateInput.fill(TEST_DATE);
             const screenshot = await page.screenshot({ fullPage: true });
             expect(screenshot).toMatchSnapshot(`print-homepage-${colorScheme}.png`);
         });
