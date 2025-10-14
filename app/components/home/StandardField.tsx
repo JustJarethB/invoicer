64c5024cb337ba216e2f262dad23e49788851db7
Jareth Bower <31932204+JustJarethB@users.noreply.github.com>
2025-10-14 11:53:44 +0100

    refactor: field formatting - moved normalisedField function outside BankField. It does not depend on any rendertime value so we do not need to recreate it with each render - converted normaliseField to a curry. This is mostly just for visual candy. - added `formatOnChange` prop to inputs. I don't think it's a good idea to have "side effects" for onChangee based on return type, so I've given formatting its own optional prop

--- app/components/home/StandardField.tsx
+++ app/components/home/StandardField.tsx
@@ -1,4 +1,4 @@
-import { useMemo, type ComponentPropsWithoutRef } from "react";
+import { useCallback, useMemo, type ComponentPropsWithoutRef } from "react";
 import { TextInput } from "~/components/Inputs";
 
 export const StandardField = ({ title, className, parentClass, ...rest }: ComponentPropsWithoutRef<typeof TextInput> & { title: string; parentClass?: string; }) => (
@@ -9,25 +9,26 @@ export const StandardField = ({ title, className, parentClass, ...rest }: Compon
 
 );
 
-export const BankField = ({ title, className, parentClass, type, ...rest }: ComponentPropsWithoutRef<typeof TextInput> & { title: string; parentClass?: string; type: "sort" | "account"}) => {
-    const reg = useMemo(() => (
-        type == "sort" ? { grouping: 2, maxChars: 8 }
-        : type == "account" ? { grouping: 4, maxChars: 9 }
-        : { grouping: undefined, maxChars: undefined }),
-        [type]);
-
-    const normaliseField = (value: string, grouping: number | undefined) => {
-        if (!grouping) return value
-        const regex = new RegExp(`.{1,${grouping}}`, "g");
-        return value
+const normaliseField = (grouping: number | undefined) => (value: string,) => {
+    if (!grouping) return value
+    const regex = new RegExp(`.{1,${grouping}}`, "g");
+    return value
         .replace(/\s+/g, "")
         .match(regex)
         ?.join(" ")
         ?? value
-    }
+}
+
+export const BankField = ({ title, className, parentClass, type, ...rest }: ComponentPropsWithoutRef<typeof TextInput> & { title: string; parentClass?: string; type: "sort" | "account" }) => {
+    const reg = useMemo(() => (
+        type == "sort" ? { grouping: 2, maxChars: 8 }
+            : type == "account" ? { grouping: 4, maxChars: 9 }
+                : { grouping: undefined, maxChars: undefined }),
+        [type]);
+
 
     const placeholder = type == "sort" ? "12 12 12" : type == "account" ? "1234 1234" : undefined
-    
+    const normalise = useCallback(normaliseField(reg.grouping), [reg.grouping])
     return <div className={`flex justify-between ${parentClass}`}>
         <p className="font-bold px-2">{title}</p>
         <TextInput
@@ -35,7 +36,7 @@ export const BankField = ({ title, className, parentClass, type, ...rest }: Comp
             maxLength={reg.maxChars}
             placeholder={placeholder}
             {...rest}
-            onChange={(value) => normaliseField(value, reg.grouping)}
-            />
+            formatOnChange={normalise}
+        />
     </div>
 }
