b10b5f1160380af430ed54e37c3239cb5138b57f
Jareth Bower <31932204+JustJarethB@users.noreply.github.com>
2025-10-15 18:42:25 +0100

    refactor: input fromatting

--- app/components/home/StandardField.tsx
+++ app/components/home/StandardField.tsx
@@ -1,5 +1,5 @@
-import { useCallback, useMemo, type ComponentPropsWithoutRef } from "react";
-import { TextInput } from "~/components/Inputs";
+import { type ComponentPropsWithoutRef } from "react";
+import { formatterOf, TextInput, type Formatter } from "~/components/Inputs";
 
 export const StandardField = ({ title, className, parentClass, ...rest }: ComponentPropsWithoutRef<typeof TextInput> & { title: string; parentClass?: string; }) => (
     <div className={`flex justify-between ${parentClass}`}>
@@ -9,34 +9,14 @@ export const StandardField = ({ title, className, parentClass, ...rest }: Compon
 
 );
 
-const normaliseField = (grouping: number | undefined) => (value: string,) => {
-    if (!grouping) return value
-    const regex = new RegExp(`.{1,${grouping}}`, "g");
-    return value
-        .replace(/\s+/g, "")
-        .match(regex)
-        ?.join(" ")
-        ?? value
-}
+type FieldType = 'sortCode' | 'accountNumber'
 
-export const BankField = ({ title, className, parentClass, type, ...rest }: ComponentPropsWithoutRef<typeof TextInput> & { title: string; parentClass?: string; type: "sort" | "account" }) => {
-    const reg = useMemo(() => (
-        type == "sort" ? { grouping: 2, maxChars: 8 }
-            : type == "account" ? { grouping: 4, maxChars: 9 }
-                : { grouping: undefined, maxChars: undefined }),
-        [type]);
-
-
-    const placeholder = type == "sort" ? "12 12 12" : type == "account" ? "1234 1234" : undefined
-    const normalise = useCallback(normaliseField(reg.grouping), [reg.grouping])
-    return <div className={`flex justify-between ${parentClass}`}>
-        <p className="font-bold px-2">{title}</p>
-        <TextInput
-            className={`w-1/2 ${className}`}
-            maxLength={reg.maxChars}
-            placeholder={placeholder}
-            {...rest}
-            formatOnChange={normalise}
-        />
-    </div>
+export const fieldFormattingOf = (type: FieldType): Formatter => {
+    switch (type) {
+        case "sortCode":
+            return formatterOf({ maxChars: 6, grouping: 2, spacer: "-" })
+        case "accountNumber":
+            return formatterOf({ maxChars: 8, grouping: 4, spacer: " " })
+        default: return { maxLength: undefined, formatOnChange: undefined }
+    }
 }
