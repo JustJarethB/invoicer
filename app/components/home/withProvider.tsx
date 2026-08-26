03e484f4f6a62b7a1a142218fe365179ab9a4a44
Jareth <JustJarethB@gmail.com>
2026-08-26 23:08:44 +0100

    refactor: remove explicit any types (#31)

--- app/components/home/withProvider.tsx
+++ app/components/home/withProvider.tsx
@@ -1,7 +1,7 @@
 import type { ReactNode } from "react";
 
-export const withProvider = (Provider: React.ComponentType<{ children: ReactNode; }>) => (Component: React.ComponentType<any>) => {
-    return function WrappedComponent(props: any) {
+export const withProvider = (Provider: React.ComponentType<{ children: ReactNode; }>) => <P extends object>(Component: React.ComponentType<P>) => {
+    return function WrappedComponent(props: P) {
         return (
             <Provider>
                 <Component {...props} />
